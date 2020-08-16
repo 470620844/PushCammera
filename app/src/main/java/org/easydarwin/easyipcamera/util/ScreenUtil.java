ba47e0d8661d4ee64e0f935aea4c76e7de17c93b
470620844 <470620844@qq.com>
2020-08-16 20:20:03 +0800

    Initial commit

---
+++ app/src/main/java/org/easydarwin/easyipcamera/util/ScreenUtil.java
@@ -0,0 +1,49 @@
+package org.easydarwin.easyipcamera.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+/**
+ * description ：
+ * author : bjc
+ * date : 2019/4/26 17:30
+ */
+public class ScreenUtil {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * px转换sp
+     */
+    public static int px2sp(Context context, int pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * sp转换px
+     */
+    public static int sp2px(Context context, int spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+
+
+
+}
